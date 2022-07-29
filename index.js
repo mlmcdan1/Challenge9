d7f3f3e5debd9b34009b5aa8f6027b49a8e4af7a
mlmcdan1 <77369368+mlmcdan1@users.noreply.github.com>
2022-07-28 21:00:18 -0500

    new commit

---
+++ index.js
@@ -0,0 +1,100 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter your project title.");
+        return false;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "projectDescription",
+    message: "Briefly give an explanation of your project. (Required)",
+    validate: description => {
+      if (description) {
+        return true;
+      } else {
+        console.log("Please write a short description.");
+        return false;
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "installInfo",
+    message: "Provide any installation instructions here:",
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Which license is used for this project:',
+    choices: ['Apache', 'Mozilla', 'MIT', 'GNU', 'Boost', 'ISC'],
+    validate: choicesLength => {
+      if (choicesLength.length <= 1) {
+        return true;
+      } else {
+        return "Please select one license!";
+      }
+    }
+  },
+  {
+    type: "input",
+    name: "usageInfo",
+    message: "Describe the main use case for this project/application:",
+  },
+  {
+    type: "input",
+    name: "contributionsInfo",
+    message: "Are their any guidlines for contributing to this project:",
+  },
+  {
+    type: "input",
+    name: "testsInfo",
+    message: "Which tests would you like to include:",
+  },
+  {
+    type: "input",
+    name: "githubUsername",
+    message: "What is your GitHub username (Required)?",
+  },
+  {
+    type: "input",
+    name: "emailInfo",
+    message: "What is a good email someone can reach you(Required)?",
+  }];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(".README.md", generateMarkdown(data), function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('Success!');
+      });
+}
+
+// TODO: Create a function to initialize app
+function init() { 
+    inquirer.prompt(questions)
+    .then(function(answer) {
+      const fileName =
+        answer.title
+          .split(' ')
+          .join('') + '.md';
+      
+      writeToFile(fileName, answer);
+    });
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,95 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license.length === 0) {
+    return ""
+  } else if (license == "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license == "Mozilla") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } else if (license == "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license == "GNU") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license == "Boost") {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if (license == "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license.length === 0) {
+    return ""
+  } else if (license == "Apache") {
+    return `https://opensource.org/licenses/Apache-2.0`
+  } else if (license == "Mozilla") {
+    return `https://opensource.org/licenses/MPL-2.0`
+  } else if (license == "MIT") {
+    return `https://opensource.org/licenses/MIT`
+  } else if (license == "GNU") {
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  } else if (license == "Boost") {
+    return `https://www.boost.org/LICENSE_1_0.txt`
+  } else if (license == "ISC") {
+    return `https://opensource.org/licenses/ISC`
+  }
+
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license.length === 0) {
+    return ""
+  } 
+  else {
+    return `## License
+  ${renderLicenseBadge(license)}
+  
+  ${renderLicenseLink(license)} 
+    `
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## License
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.projectDescription}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+   ${data.installInfo}
+
+  ## Usage
+  ${data.usageInfo}
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributionsInfo}
+  
+  ## Tests
+  ${data.testsInfo}
+
+  
+  ## Questions
+  My GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername}) <br>
+  Email me: ${data.emailInfo}
+
+`
+}
+
+module.exports = generateMarkdown;
